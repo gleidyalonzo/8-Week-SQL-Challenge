cb7ab95243f10612b24d70dcb570afc9f4a47891
Gleidy Alonzo <95588335+gleidyalonzo@users.noreply.github.com>
2025-10-24 20:41:57 -0400

    Update case two
    Added section B

--- Case2_Pizza_Runner/case2.sql
+++ Case2_Pizza_Runner/case2.sql
@@ -112,15 +112,173 @@ FROM [pizza_runner].[customer_orders]
 GROUP BY DATENAME(WEEKDAY, order_time), DATEPART(WEEKDAY, order_time)
 ORDER BY DATEPART(WEEKDAY, order_time);
 
-
 -- B. Runner and Customer Experience
+
 -- 1) How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT DATEADD(WEEK, DATEDIFF(WEEK, 0, registration_date), 0) as signup_week_start,
+       COUNT(runner_id) as total_runners_signed_up
+FROM [pizza_runner].[runners]
+GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, registration_date), 0)
+ORDER BY signup_week_start;
+
+-- Explanation: 
+-- It calculates the start date of the week for each signup_date by using DATEADD and DATEDIFF functions, it will give you  the starting date of the week that each registration falls into.
+-- It then groups the results by this calculated week start date and counts the number of runners who signed up in each week.
+
 -- 2) What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+WITH RunnerPickupTravelTime AS (
+    SELECT DISTINCT
+        a.order_id,
+        a.runner_id,
+        a.pickup_time,
+        b.order_time,
+        DATEDIFF(minute, b.order_time, pickup_time) AS pickup_travel_time
+    FROM pizza_runner.runner_orders AS a
+    LEFT JOIN pizza_runner.customer_orders AS b
+        ON a.order_id = b.order_id
+    WHERE a.pickup_time != 'null'
+)
+SELECT
+    runner_id,
+    AVG(pickup_travel_time * 1.0) AS average_pickup_time
+FROM RunnerPickupTravelTime
+GROUP BY runner_id;
+
+-- Explanation:
+-- The CTE RunnerPickupTravelTime calculates the travel time for each order by finding the difference in minutes between the order_time and pickup_time.
+-- The main query then averages these travel times for each runner.
+-- Note: Multiplying by 1.0 ensures that the average is returned as a decimal value.
+-- pcikup_time field contains 'null' values recorded as a string, these values represent cancelled orders
+
 -- 3) Is there any relationship between the number of pizzas and how long the order takes to prepare?
+WITH OrderPizzaCounts AS (
+    SELECT 
+        a.order_id,
+        COUNT(a.pizza_id) AS pizza_count,
+        DATEDIFF(minute, b.order_time, a.pickup_time) AS preparation_time
+    FROM pizza_runner.runner_orders AS a
+    JOIN pizza_runner.customer_orders AS b
+        ON a.order_id = b.order_id
+    WHERE a.pickup_time != 'null' -- Exclude cancelled orders
+    GROUP BY a.order_id, b.order_time, a.pickup_time
+)
+SELECT 
+    pizza_count,
+    AVG(preparation_time * 1.0) AS average_preparation_time
+FROM OrderPizzaCounts
+GROUP BY pizza_count
+ORDER BY pizza_count;
+-- Explanation:
+-- The more pizza an order has, the longer it generally takes to prepare.
+-- The CTE OrderPizzaCounts calculates the number of pizzas and preparation time for each order.
+-- The main query then averages the preparation times grouped by the number of pizzas in the order.
+
 -- 4) What was the average distance travelled for each customer?
+WITH CustomerDistances AS (
+    SELECT 
+        a.order_id,
+        b.customer_id,
+        CAST(REPLACE(a.distance, 'km', '') AS FLOAT) AS distance_km
+    FROM pizza_runner.runner_orders AS a
+    JOIN pizza_runner.customer_orders AS b
+        ON a.order_id = b.order_id
+    WHERE a.pickup_time != 'null' -- Exclude cancelled orders
+)
+SELECT 
+    customer_id,
+    AVG(distance_km * 0.1) AS average_distance_km
+FROM CustomerDistances
+GROUP BY customer_id
+ORDER BY customer_id;
+-- Explanation:
+-- The CTE CustomerDistances extracts the numeric distance in kilometers for each order by removing the '
+-- km' suffix and converting it to a FLOAT.
+-- The main query then averages these distances for each customer.
+--  Note: The distance field contains 'null' values recorded as a string, these values represent cancelled orders
+
 -- 5) What was the difference between the longest and shortest delivery times for all orders?
+
+WITH DeliveryTimes AS (
+    SELECT 
+        order_id,
+        TRY_CAST(
+            REPLACE(
+                TRANSLATE(
+                    duration,
+                    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                    '                                                    ' -- 52 spaces
+                ),
+                ' ',
+                ''
+            ) AS INT
+        ) AS delivery_time
+    FROM [pizza_runner].[runner_orders]
+    WHERE pickup_time <> 'null'
+)
+SELECT 
+    MAX(delivery_time) - MIN(delivery_time) AS delivery_time_difference
+FROM DeliveryTimes;
+-- Explanation:
+-- The CTE DeliveryTimes extracts the numeric delivery time in minutes for each order by removing all alphabetic characters from the duration field.
+-- The main query then calculates the difference between the maximum and minimum delivery times.
+-- Note: The pickup_time field contains 'null' values recorded as a string, these values represent cancelled orders
+-- REPLACE is used to clean the duration field to extract numeric values, the TRANSLATE is used to remove all alphabetic characters.
+-- try_cast is used to handle any non-numeric values that may arise after cleaning the duration field.
+
 -- 6) What was the average speed for each runner for each delivery and do you notice any trend for these values?
+WITH RunnerSpeeds AS (
+    SELECT 
+        order_id,
+        runner_id,
+        CAST(REPLACE(trim(distance), 'km', '') AS FLOAT) AS distance_km,
+       REPLACE(
+        TRANSLATE(
+            duration,
+            'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            '                                                    ' -- 52 spaces
+        ),
+        ' ',
+        '')  as delivery_time 
+    FROM [pizza_runner].[runner_orders]
+    WHERE pickup_time != 'null' -- Exclude cancelled orders
+)
+SELECT 
+    runner_id,
+    order_id,
+    distance_km,
+    delivery_time,
+   (distance_km / delivery_time) * 60  AS average_speed_kmh
+FROM RunnerSpeeds
+ORDER BY runner_id, order_id;
+-- Explanation:
+-- The CTE RunnerSpeeds extracts the numeric distance in kilometers and delivery time in minutes for each order by removing the 'km' suffix from distance and stripping non-numeric characters from duration.
+-- The main query then calculates the average speed in km/h for each delivery by dividing distance by delivery time (converted to hours).
+-- Note: The pickup_time field contains 'null' values recorded as a string, these values represent cancelled orders
+-- REPLACE is used to clean the duration field to extract numeric values, the TRANSLATE is used to remove all alphabetic characters.
+
 -- 7) What is the successful delivery percentage for each runner?
+WITH RunnerDeliveryStats AS (
+    SELECT 
+        runner_id,
+        COUNT(order_id) AS total_orders,
+        SUM(CASE WHEN pickup_time != 'null' THEN 1 ELSE 0 END) AS successful_deliveries
+    FROM [pizza_runner].[runner_orders]
+    GROUP BY runner_id
+)
+SELECT 
+    runner_id,
+    successful_deliveries,
+    total_orders,
+    (successful_deliveries * 1.0 / total_orders) * 100 AS successful_delivery_percentage
+FROM RunnerDeliveryStats
+ORDER BY runner_id;
+
+-- Explanation:
+-- The CTE RunnerDeliveryStats calculates the total number of orders and successful deliveries for each runner.
+-- The main query then calculates the successful delivery percentage by dividing successful deliveries by total orders and multiplying
+-- by 100 to get a percentage.
+-- whats considered a successful delivery is determined by whether pickup_time is not equal to 'null' (string), which indicates the order was not cancelled.
 
 -- C. Ingredient Optimisation
 -- 1) What are the standard ingredients for each pizza?
